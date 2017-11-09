4e8d541ecbde93f3c29594e7ebe556a6a143a7b5
mngkvn <kmowebdeveloper@outlook.com>
2017-11-09 11:18:22 -0600

    Added Forms and Video Request Entity

---
+++ src/AppBundle/Entity/RequestEntity/VideoRequestEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KevinOtto
+ * Date: 11/9/2017
+ * Time: 8:30 AM
+ */
+
+namespace AppBundle\Entity\RequestEntity;
+
+
+class VideoRequestEntity
+{
+
+}
---
+++ src/AppBundle/Form/PhotoRequestForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KevinOtto
+ * Date: 11/9/2017
+ * Time: 9:29 AM
+ */
+
+namespace AppBundle\Form;
+
+
+class PhotoRequestForm
+{
+
+}
